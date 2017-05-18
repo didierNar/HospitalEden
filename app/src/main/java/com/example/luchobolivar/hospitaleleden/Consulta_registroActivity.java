2cdfac680ad5d8fc400bbff295e256633011b044
Lucho <tennluis77@gmail.com>
2017-05-18 11:31:55 -0500

    commit 3

--- app/src/main/java/com/example/luchobolivar/hospitaleleden/Consulta_registroActivity.java
+++ app/src/main/java/com/example/luchobolivar/hospitaleleden/Consulta_registroActivity.java
@@ -3,25 +3,70 @@ package com.example.luchobolivar.hospitaleleden;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Consulta_registroActivity extends AppCompatActivity {
 
-    ImageButton imagenConsulta;
+    Spinner tipoCita;
+    Spinner espe;
+    ArrayAdapter<CharSequence> adapterTipoCita;
+    ArrayAdapter<CharSequence> adapterEspe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta_registro);
 
-        imagenConsulta = (ImageButton) findViewById(R.id.btnConsultaCitas);
+        espe = (Spinner) findViewById(R.id.cbEspecializaciones);
+        tipoCita = (Spinner) findViewById(R.id.cbTipoCita);
+        adapterTipoCita = ArrayAdapter.createFromResource(this, R.array.tipoCita, android.R.layout.simple_spinner_item);
+        adapterTipoCita.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tipoCita.setAdapter(adapterTipoCita);
 
-        imagenConsulta.setOnClickListener(new View.OnClickListener() {
-
+        tipoCita.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if(position == 3 || position == 1){
+                    especializacionesCombo();
+                } else if(position == 2){
+                    espe.setAdapter(null);
+                }
 
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
         });
+
+
     }
+
+    public void especializacionesCombo(){
+
+        adapterEspe = ArrayAdapter.createFromResource(this, R.array.tipoEspe, android.R.layout.simple_spinner_item);
+        adapterEspe.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        espe.setAdapter(adapterEspe);
+
+    }
+
+    public void listarMedicosSedes(View view){
+
+
+
+    }
+
+
+
+
+
+
+
+
 }
